7909f98c8c1d47667da794d50ff449d1b23eb5e2
omsatam <62635289+omsatam@users.noreply.github.com>
2020-08-18 18:52:39 +0530

    update email

--- check price and notify with email/pricefall_email.py
+++ check price and notify with email/pricefall_email.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
+#url for web scrapping
 url = 'https://www.amazon.in/Redmi-Note-Pro-Storage-Processor/dp/B07X4PKGSN'
 
 headers = {'user agent':'Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36'}
+
+
+
 def check_price():
+	#function to find price and comparing with our set price
 	web = requests.get(url,headers = headers).text
 	soup = BeautifulSoup(web,'html.parser')
 	soup.prettify()
@@ -14,23 +19,25 @@ def check_price():
 	print(price)
 	actual_price = float(price[2:4])
 	print(actual_price)
-	if actual_price > 15 :
+	if actual_price < 15 :
 		send_mail()
 
+		
 def send_mail():
+	#function for sending email to respective person
 	server = smtplib.SMTP('smtp.gmail.com',587)
 	server.ehlo()
 	server.starttls()
 	server.ehlo()
 	
-	server.login('satamom254@gmail.com','ydcpkkzqtdwndcqs')
+	server.login('MyEmail','MyPassword')
 	subject = 'Price fell down'
 	body = 'Hey the product you are looking for is now in your budget. Check the link given below  https://www.amazon.in/Redmi-Note-Pro-Storage-Processor/dp/B07X4PKGSN'
 	
 	message = f'subject:{subject} \n\n {body}'
 	server.sendmail(
-	'satamom254@gmail.com',
-	'omsatam1005@gmail.com',
+	'sender_email',
+	'reciever_email',
 	message
 	)
 	print('Email has been sent successfully')
